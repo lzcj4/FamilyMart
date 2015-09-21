4b3d91a30cc5597257ae9ee64dd6258073b10a90
lzcj4 <lzcj4@163.com>
2015-09-21 17:33:17 +0800

    improve accurate for statistic chart

--- FamilyMartUI/UC/MyCanvas.cs
+++ FamilyMartUI/UC/MyCanvas.cs
@@ -12,13 +12,13 @@ namespace FamilyMartUI.UC
     public class MyCanvas : Canvas
     {
         private const double lineWidth = 1.5;
-        private const double arrowWidth = 3;
+        private const double pointRadius = 2;
         private const double flagHeight = 10;
-        private const double flagOffset = 2;
-
-        private const double radius = 2;
+        private const double offset = 5;
         private const string fontFace = "Segoe UI";
 
+        private double yChartPadding = 20;
+        private double xChartPadding = 20;
         int[] xPoints = { 1, 2, 3, 4, 5, 6, 7 };
         int[] yPoints = { 10, 20, 30, 40, 50, 60, 70 };
         double[][] dataArray = new double[][]{ new double[] { 15, 23, 56, 34, 20, 44, 2 }, 
@@ -37,6 +37,8 @@ namespace FamilyMartUI.UC
             this.xPoints = xDates.Select(item => item.Day).ToArray();
             this.yPoints = yPoints;
             this.dataArray = data;
+            this.xChartPadding = 20;
+            this.yChartPadding = 20;
             this.InvalidateVisual();
         }
 
@@ -69,67 +71,49 @@ namespace FamilyMartUI.UC
 
             Pen blackPen = new Pen(Brushes.Black, lineWidth);
             Pen grayPen = new Pen(Brushes.LightGray, 1);
-            Pen arrowPen = new Pen(Brushes.Black, arrowWidth);
 
             FormattedText xMaxFT = GetFormattedText(xPoints.Max().ToString());
             FormattedText yMaxFT = GetFormattedText(yPoints.Max().ToString());
             FormattedText tMaxFT = GetFormattedText(titles.Max().ToString());
 
-            double xOffset = 2 * yMaxFT.Width + flagOffset;
-            double yOffset = xMaxFT.Height + flagOffset;
-            double canvasWidth = this.ActualWidth - xOffset - tMaxFT.Width;
-            double canvasHeight = this.ActualHeight - yOffset - flagHeight;
+            xChartPadding = yMaxFT.Width > xChartPadding ? yMaxFT.Width + offset : xChartPadding;
+            yChartPadding = xMaxFT.Height > yChartPadding ? xMaxFT.Height : yChartPadding;
+
+            double canvasWidth = this.ActualWidth - xChartPadding * 2;
+            double canvasHeight = this.ActualHeight - yChartPadding * 2;
 
             double xStep = canvasWidth / xPoints.Length;
-            double yStep = canvasHeight / yPoints.Length;
             int j = 1;
-            dc.DrawLine(blackPen, new Point(xOffset, canvasHeight), new Point(xOffset + canvasWidth, canvasHeight));
+            dc.DrawLine(blackPen, new Point(xChartPadding, canvasHeight + yChartPadding),
+                                  new Point(xChartPadding + canvasWidth, canvasHeight + yChartPadding));
             FormattedText ftStart = GetFormattedText("0");
+            dc.DrawText(ftStart, new Point(yChartPadding, canvasHeight + yChartPadding - flagHeight / 4));
 
-            dc.DrawText(ftStart, new Point(xOffset, canvasHeight + flagOffset));
             //X-order flag
             for (int i = 0; i < xPoints.Length; i++, j++)
             {
-                double startX = xOffset + j * xStep;
+                double startX = xChartPadding + j * xStep;
                 DateTime itemDate = xPointsDates[i];
                 FormattedText ft = GetFormattedText(xPoints[i].ToString(),
                                                    (itemDate.DayOfWeek == DayOfWeek.Sunday ||
                                                     itemDate.DayOfWeek == DayOfWeek.Saturday) ? Brushes.Red : Brushes.Black);
-                //if (i == xPoints.Length - 1)
-                //{
-                //    //arrow
-                //    dc.DrawLine(arrowPen, new Point(startX - flagHeight * 2, canvasHeight - flagHeight),
-                //                new Point(startX, canvasHeight));
-                //    dc.DrawText(ft, new Point(startX - ft.Width, canvasHeight + flagOffset));
-                //}
-                //else
-                {
-                    dc.DrawLine(blackPen, new Point(startX, canvasHeight), new Point(startX, canvasHeight - flagHeight));
-                    dc.DrawText(ft, new Point(startX - ft.Width / 2, canvasHeight + flagOffset));
-                }
+                dc.DrawLine(blackPen, new Point(startX, canvasHeight + yChartPadding),
+                                      new Point(startX, canvasHeight + yChartPadding - flagHeight));
+                dc.DrawText(ft, new Point(startX - ft.Width / 2, canvasHeight + yChartPadding - flagHeight / 4));
             }
 
-            dc.DrawLine(blackPen, new Point(xOffset, canvasHeight), new Point(xOffset, flagHeight));
+            dc.DrawLine(blackPen, new Point(xChartPadding, canvasHeight + yChartPadding),
+                                  new Point(xChartPadding, yChartPadding));
             j = 1;
 
-            ;
+            double yStep = canvasHeight / yPoints.Length;
             //Y-order flag
             for (int i = 0; i < yPoints.Length; i++, j++)
             {
-                double startY = j * yStep - flagHeight;
+                double startY = j * yStep;
                 FormattedText ft = GetFormattedText(yPoints[i].ToString());
-                //if (i == yPoints.Length - 1)
-                //{
-                //    dc.DrawLine(arrowPen, new Point(xOffset + flagHeight, flagHeight * 3),
-                //                new Point(xOffset, flagHeight));
-                //    dc.DrawText(ft, new Point(yMaxFT.Width / 2, canvasHeight - startY - ft.Height / 2));
-                //}
-                //else
-                {
-                    dc.DrawLine(grayPen, new Point(xOffset, canvasHeight - startY), new Point(canvasWidth + xOffset, canvasHeight - startY));
-                    dc.DrawText(ft, new Point(yMaxFT.Width / 2, canvasHeight - startY - ft.Height / 2));
-                }
-
+                dc.DrawLine(grayPen, new Point(xChartPadding, canvasHeight + yChartPadding - startY), new Point(canvasWidth + xChartPadding, canvasHeight + yChartPadding - startY));
+                dc.DrawText(ft, new Point(0, canvasHeight + yChartPadding - startY - ft.Height / 2));
             }
 
             //divide to the every value step
@@ -139,31 +123,25 @@ namespace FamilyMartUI.UC
                 //Line
                 j = 1;
                 Pen currentPen = linePens[i];
-                Point lastPoint = new Point(xOffset, canvasHeight);
+                Point lastPoint = new Point(xChartPadding, canvasHeight + yChartPadding);
 
                 for (int z = 0; z <= dataArray[0].GetUpperBound(0); z++, j++)
                 {
-                    //Point newPoint = new Point(xOffset + j * xStep - radius, canvasHeight + flagHeight - dataArray[i][z] * yStep - radius);
-                    Point newPoint = new Point(xOffset + j * xStep - radius, canvasHeight - dataArray[i][z] * yStep - radius);
+                    Point newPoint = new Point(xChartPadding + j * xStep,
+                                               canvasHeight + yChartPadding - dataArray[i][z] * yStep);
                     dc.DrawLine(currentPen, lastPoint, newPoint);
-                    dc.DrawEllipse(currentPen.Brush, currentPen, newPoint, radius, radius);
+                    dc.DrawEllipse(currentPen.Brush, currentPen, newPoint, pointRadius, pointRadius);
                     lastPoint = newPoint;
                 }
             }
 
-            double yMiddle = canvasHeight / 2;
+            double yMiddle = this.ActualHeight / 2;
 
             int middleValue = (int)Math.Ceiling(titles.Length * 1.0 / 2);
             for (int i = 0; i < titles.Length; i++)
             {
                 FormattedText ft = GetFormattedText(titles[i], linePens[i].Brush);
-                dc.DrawText(ft, new Point(xOffset + canvasWidth, yMiddle - 4 * flagHeight * middleValue--));
-                //if (i == 0)
-                //    dc.DrawText(ft, new Point(xOffset + canvasWidth, yMiddle - 4 * flagHeight));
-                //if (i == 1)
-                //    dc.DrawText(ft, new Point(xOffset + canvasWidth, yMiddle));
-                //if (i == 2)
-                //    dc.DrawText(ft, new Point(xOffset + canvasWidth, yMiddle + 4 * flagHeight));
+                dc.DrawText(ft, new Point(xChartPadding + canvasWidth + offset, yMiddle - 2 * yChartPadding * (--middleValue)));
             }
         }
 
